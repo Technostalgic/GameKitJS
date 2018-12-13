9f786089764829ac29183f55c0cb3e2f00106fa0
Technsotalgic <isaiahnsmith@hotmail.com>
2018-12-13 13:04:58 -0800

    added some more to spritefont

--- js/SpriteFont.js
+++ js/SpriteFont.js
@@ -12,19 +12,20 @@ class SpriteFont{
 		this.maxSpriteHeight = 16;
 	}
 
+	/** maps a character to the specified sprite rectangle */
 	mapCharacter(/**@type {string}*/char, /**@type {rect}*/ sprite){
 
 		this.spriteTable[char.charCodeAt[0]] = sprite;
 	}
 
-	/** render's a string using the sprite characters from the SpriteFont sprite sheet */
+	/** renders a string using the sprite characters from the SpriteFont sprite sheet */
 	drawString(/** @type {CanvasRenderingContext2D} */ ctx, /**@type {string}*/ str){
 
 		let pos = new vec2();
 
 		sprites = [];
 		for(let i = 0; i < str.length; i++){
-			let sprite = new rect(pos, )
+			let sprite = this.spriteTable[str.charCodeAt[i]];
 		}
 	}
 }
