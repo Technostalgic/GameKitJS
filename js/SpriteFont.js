c23b011e0026b16bbdc6b21782efa36b3058d63a
Technostalgic <Isaiahnsmith@hotmail.com>
2019-01-14 10:32:47 -0800

    fixed bug where spritefont image generation wasn't working correctly

--- js/SpriteFont.js
+++ js/SpriteFont.js
@@ -58,35 +58,29 @@ class SpriteFont {
 	 * sprite sheet onto a canvas and returns it */
 	GenerateTextImage(/**@type {string}*/ str, /**@type {TextRenderFormat}*/ format) {
 
-		// TODO: THIS WHOLE FUCKING FUNCTION IS WONKY, FIX IT!
-
+		// create the canvas object that will contain the text image
 		var tcanvas = document.createElement("canvas");
 		tcanvas.width = str.length * this.maxSpriteWidth;
 		tcanvas.height = this.maxSpriteHeight;
 		var ctx = tcanvas.getContext("2d");
+		
+		ctx.fillStyle = "#000";
+		ctx.fillRect(0, 0, 100, 100);
 
-		// TODO: Fix temporary patch (ACTUALLY NEVER MIND IT TURNS OUT THE BS PATCH DOESN'T EVEN WORK)
-		// this code (specifically drawing to the 'ctx' context) doesn't work unless called 
-		// asynchronously for some reason and I can't figure out why. for now, I'll just call 
-		// it asynchronously and hope that a solution pops into my head at some point
-		var ths = this;
-		setTimeout(function(){
-			
-			// for debugging: writing to canvas only seems to work if it doesn't go any further than these two lines
-			ctx.fillStyle = "#000";
-			ctx.fillRect(0, 0, 100, 100);
-			// return
-
-			let cwidth = 0;
-			for(let i = 0; i < str.length; i++){
-				let sprite = ths.spriteTable[str.charCodeAt(i)];
-				let target = new rect(new vec2(cwidth, 0), sprite.size.clone)
-				RenderHelper.DrawSprite(ctx, ths.spriteSheet, target, sprite);
-				cwidth += sprite.width;
-			}
-
-			tcanvas.width = cwidth;
-		});
+		// determine the width of the canvas and set it before anything is rendered onto the canvas
+		var cwidth = 0;
+		for(let i = 0; i < str.length; i++)
+			cwidth += this.spriteTable[str.charCodeAt(i)].width;
+		tcanvas.width = cwidth;
+		
+		// render each character in the string onto the canvas
+		cwidth = 0;
+		for(let i = 0; i < str.length; i++){
+			let sprite = this.spriteTable[str.charCodeAt(i)];
+			let target = new rect(new vec2(cwidth, 0), sprite.size.clone)
+			RenderHelper.DrawSprite(ctx, this.spriteSheet, target, sprite);
+			cwidth += sprite.width;
+		}
 		
 		return tcanvas;
 	}
